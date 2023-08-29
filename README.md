0a5513cfd3f5c0efccebf98c8f0aec46894d66ad
jasperfan <jasper@torchtechpro.com>
2023-08-29 17:28:24 +0800

    修正算法

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # expected_cost
 期望值計算
+## 題目
+let the initial success rate to be X, the cost of each try to be Y and the increament of success rate after failure to be Z.
+Try to find calculate the expected times of getting the first success.
+## 範例
+```
+X = 0.5
+Y = 10
+Z = 0.16
+``` 
+## 解答
+```
+Expected cost = 17.0164
+```
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,80 @@
+use std::fs;
+use toml::Value;
+fn init_from_toml() -> (f64, f64, f64) {
+    let toml_content = fs::read_to_string("condition.toml").expect("Unable to read file");
+    let parsed_content: Value = toml::from_str(&toml_content).expect("Unable to parse TOML");
+    let toml_value = Value::try_from(parsed_content).unwrap();
+    //print!("{:?}", toml_value);
+    // 初始化參數
+    let init_success_rate = toml_value["condition"]["init_success_rate"]
+        .as_float()
+        .unwrap(); // 初始成功率
+    let success_increase = toml_value["condition"]["success_increase"]
+        .as_float()
+        .unwrap(); // 成功率增加量
+    let single_shot_cost = toml_value["condition"]["single_shot_cost"]
+        .as_float()
+        .unwrap(); // 單次升級消耗
+
+    (init_success_rate, success_increase, single_shot_cost)
+}
+
 fn calculate_expected_cost(
     accumulated_success_rate: f64,
     chance_to_this_stage: f64,
     failure_increase: f64,
-    single_shot_cost: f64,
-    current_cost: f64,
+    expected_trial_times: f64,
     current_loop: i32,
 ) -> f64 {
     println!(
-        "第{}次升級，成功率: {:.2}%，目前累計{:.4}",
+        "第{}次升級，成功率: {:.4}%，目前累計{:.4}",
         current_loop,
         accumulated_success_rate * 100.0,
-        current_cost
+        expected_trial_times
     );
-
-    let failure_rate = 1.00 - accumulated_success_rate as f64;
-    let current_stage_cost = single_shot_cost * current_loop as f64 * chance_to_this_stage;
-    println!("成功機率: {:.2}%", accumulated_success_rate * 100.0);
-    println!("本次消耗: {:.2}", current_stage_cost);
-    let expected_cost = accumulated_success_rate * current_stage_cost; // 成功的期望值
-    println!("成功的期望值: {:.2}", expected_cost);
-    let current_cost = current_cost + expected_cost; // 累計消耗 + 本次消耗
+    // 計算每輪的期望值 = 進入此輪機率 * 本次成功機率 * 迴圈數
+    // 進入此輪機率 =
+    println!("進入本輪機率: {:.4}%", chance_to_this_stage * 100.0);
+    println!("本輪成功機率: {:.4}%", accumulated_success_rate * 100.0);
+    let expected_cost = chance_to_this_stage * accumulated_success_rate * current_loop as f64; // 此輪中止的次數期望值
+    println!("成功的期望值: {:.4}", expected_cost);
+    let current_cost = expected_trial_times + expected_cost; // 累計消耗 + 本次消耗
     if accumulated_success_rate >= 1.0 {
         println!("已達到100%成功率，不會再失敗");
         println!("累計消耗: {:.4}", current_cost);
         return current_cost;
     }
+    let failure_rate = 1.00 - accumulated_success_rate as f64; // 此輪失敗率
+    let rate_to_next_stage = chance_to_this_stage * failure_rate; // 進入下一輪機率
+    println!("此輪進入下輪機率: {:.4}%", failure_rate * 100.0);
+    println!("全域進入下輪機率: {:.4}%", rate_to_next_stage * 100.0);
     // Recursively calculate the expected cost for the next upgrade
-    let next_round_rate = accumulated_success_rate + failure_increase; // 進入下一輪機率
+    let next_round_rate = accumulated_success_rate + failure_increase; // 下一輪成功率
     calculate_expected_cost(
         next_round_rate,
-        failure_rate * chance_to_this_stage,
+        rate_to_next_stage,
         failure_increase,
-        single_shot_cost,
         current_cost,
         current_loop + 1,
     )
 }
 
 fn main() {
-    let init_success_rate = 0.5; // 初始成功率
-    let success_increase = 0.16; // 成功率增加量
-    let single_shot_cost: f64 = 10.0; // 單次升級消耗
-    let current_cost: f64 = 0.0; // 當前消耗
-    let current_loop: i32 = 1; // 當前升級次數
+    // 改從toml讀取
+    let (init_success_rate, success_increase, single_shot_cost) = init_from_toml();
+    let expected_trial_times = 0.0; // 預期試驗次數
+    let current_loop: i32 = 1; // 當前次數
 
     let expected_cost = calculate_expected_cost(
         init_success_rate,
         1.0,
         success_increase,
-        single_shot_cost,
-        current_cost,
+        expected_trial_times,
         current_loop,
     );
-    println!("總共消耗的期望值: {:.4}", expected_cost);
+    println!(
+        "次數期望值: {:.4}，總共消耗的期望值: {:.4}",
+        expected_cost,
+        expected_cost * single_shot_cost as f64
+    );
 }
